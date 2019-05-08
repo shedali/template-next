148abb3b25cd91c92b0ceb4bf567170c744bed6b
shedali <shedali@users.noreply.github.com>
2019-05-08 01:36:25 +0100

    invoice structure

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,27 @@
 import Link from 'next/link'
 export default () => (
   <div>
-      <h1>SHEDALI</h1>
-
-
-  This is an attempt to create a pdf invoice ß
+      
+      
+      COMPANY DETAILS
+      
+      INVOICE NUMBER
+      
+      INVOCE TO 
+      
+      ITEMISED
+      
+      ITEM
+      ITEM
+      
+      SUBTOTAL
+      
+      VAT
+      
+      TOTAL
+      
+      
+      
+      
   </div>
 )
